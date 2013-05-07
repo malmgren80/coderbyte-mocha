a2d522a8592c79b6bb97f5644fea6cdef10808db
malmgren80 <marcusmalmgren@hotmail.com>
2013-05-07 23:16:00 +0200

    Completed challenge letterChanges

--- coderbyte.js
+++ coderbyte.js
@@ -8,3 +8,33 @@ exports.firstFactorial = function(num) {
 
     return sum;
 };
+
+exports.letterChanges = function(str) {
+	var modifiedStr = '';
+
+	var nextLetter = function(character) {
+		var letters = 'abcdefghijklmnopqrstuvwxyz';
+		var vowels = 'aeiou';
+
+		var lowerCaseChar = character.toLowerCase();
+		var index = letters.indexOf(lowerCaseChar);
+		if (index < 0) {
+			return character;
+		}
+
+		var newChar = letters[lowerCaseChar === 'z' ? 0 : index + 1];
+
+		var vowelIndex = vowels.indexOf(newChar);
+		if (vowelIndex >= 0) {
+			return vowels[vowelIndex].toUpperCase();
+		}
+
+		return newChar;
+	} 
+
+	for (var character in str) {
+		modifiedStr += nextLetter(str[character]);
+	}	
+
+	return modifiedStr;
+};
--- test/coderbyteTests.js
+++ test/coderbyteTests.js
@@ -2,13 +2,36 @@ var should = require('should'),
     coderbyte = require('../coderbyte.js');
 
 describe('coderbyte', function(){
-    describe('#firstFactorial(value)', function(){
-        it('should return factors multiplied', function(){
+    describe('firstFactorial', function(){
+        it('should return factors of 4 multiplied', function(){
             coderbyte.firstFactorial(4).should.equal(24);
         });
 
-        it('should return factors multiplied', function(){
+        it('should return factors of 8 multiplied', function(){
             coderbyte.firstFactorial(8).should.equal(40320);
         });
     });
+
+    describe('letterChanges', function(){
+	    it('should return next letter', function(){
+	        coderbyte.letterChanges('abc').should.equal('bcd');
+	    });
+
+	    it('should turn around on z', function(){
+	        coderbyte.letterChanges('z').should.equal('A');
+	    });
+
+	    it('should uppercase letters that becomes vowels', function(){
+	        coderbyte.letterChanges('zdhnt').should.equal('AEIOU');
+	    });
+
+	    it('string with non letters, should not modify non letters', function(){
+	        coderbyte.letterChanges('hello*3').should.equal('Ifmmp*3');
+	    });
+
+	    it('string with non letters, should not modify non letters', function(){
+	        coderbyte.letterChanges('fun times!').should.equal('gvO Ujnft!');
+	    });
+	});
 });
+
